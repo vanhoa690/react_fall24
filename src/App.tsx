658b045429759002c04c4291a3cb8c6afe25ad68
vanhoa690 <vanhoa690@gmail.com>
2024-09-12 14:38:23 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,136 +1,26 @@
-import "./App.css";
+import { useRoutes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import ClientLayout from "./layouts/ClientLayout";
 
-import { message, Popconfirm, Table, Typography } from "antd";
-import type { FormProps, PopconfirmProps } from "antd";
-import { Flex, Button, Checkbox, Form, Input } from "antd";
+const routeConfig = [
+  {
+    path: "/",
+    element: <ClientLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      { path: "about", element: <About /> },
+    ],
+  },
+];
 
-const { Title, Text, Link, Paragraph } = Typography;
 function App() {
-  type FieldType = {
-    username?: string;
-    password?: string;
-    remember?: string;
-  };
+  const routes = useRoutes(routeConfig);
 
-  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-    console.log("Success:", values);
-  };
-
-  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
-    errorInfo
-  ) => {
-    console.log("Failed:", errorInfo);
-  };
-  const dataSource = [
-    {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
-    },
-    {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
-    },
-  ];
-
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-  ];
-  const confirm: PopconfirmProps["onConfirm"] = (e) => {
-    console.log(e);
-    message.success("Click on Yes");
-  };
-
-  const cancel: PopconfirmProps["onCancel"] = (e) => {
-    console.log(e);
-    message.error("Click on No");
-  };
-  return (
-    <Flex gap="small" wrap>
-      <Table dataSource={dataSource} columns={columns} />
-      <Popconfirm
-        title="Delete the task"
-        description="Are you sure to delete this task?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-      >
-        <Button danger>Delete</Button>
-      </Popconfirm>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      <Title>h1. Ant Design</Title>
-      <Text>Ant Design (default)</Text>
-      <Link href="https://ant.design" target="_blank">
-        Ant Design (Link)
-      </Link>
-      <Paragraph copyable>This is a copyable text.</Paragraph>
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
-      <Button type="primary" loading iconPosition={"start"}>
-        Loading
-      </Button>
-    </Flex>
-  );
+  return <main>{routes}</main>;
 }
 
 export default App;
