c9b214210f8243b72dac7c6d86e470463c7e7c06
vanhoa690 <vanhoa690@gmail.com>
2024-09-19 19:45:12 +0700

    add token headers

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+
 export const http = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : null,
+  },
 });
